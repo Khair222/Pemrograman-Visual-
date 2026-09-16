53383dbff7dece1961d361b9d09fc1f6b249a0da
fathul khair <fathulkhair393@gmail.com>
2026-09-16 12:14:52 +0700

    Modul,Fungsi,Array

---
+++ Tugas1/pertemuan4-Modul-Fungsi-Array/pertemuan4-Modul-Fungsi-Array/ModArray.vb
@@ -0,0 +1,21 @@
+﻿Module ModArray
+    Public Sub TampilkanPesan(nama As String)
+        MessageBox.Show("hallo " & nama)
+    End Sub
+
+
+    Function Hitung(panjang As Integer, lebar As Integer) As Integer
+        If panjang > 50 Then
+            Return panjang * lebar
+        Else
+            Return 2 * panjang + 2 * lebar
+        End If
+    End Function
+
+    Public nilai() As Integer = {50, 60, 70, 80, 90, 100}
+    Public nilai2D(,) As Integer = {
+        {55, 50, 60},
+        {65, 67, 69},
+        {78, 85, 99}
+        }
+End Module
